4a2a8abadd7f47dd18d5680124903a9a878fcb84
Ankit Jayswal <ankit.jayswal@engagely.ai>
2023-03-16 19:29:23 +0530

    changing response for dial_sip app and tested noanswer case

--- api/flask_cpass.py
+++ api/flask_cpass.py
@@ -27,11 +27,12 @@ def incall_api():
 def process_digits_api():
     if(request.method=='POST'):
         some_json = request.get_json()
+        print(some_json)
         digits = some_json['digits']
         if(digits=='1'):
             #res = {"response": {"app": "say","refresh": "1","text": "you have pressed one"}}
             #res = {"response": {"app": "dial_number","refresh": "1","numbers": "9979272423","action":"http://localhost:5005/api/dial_number_action","timeout":"60"}}
-            res = {"response": {"app": "dial_sip","refresh": "1","agents": "102,103","action":"http://localhost:5005/api/dial_sip_action","timeout":"60"}}
+            res = {"response": {"app": "dial_sip","refresh": "1","agents": "102","action":"http://localhost:5005/api/dial_sip_action","timeout":"10"}}
             return jsonify(res)
         elif(digits=='2'):
             res = {"response": {"app": "say","refresh": "1","text": "you have pressed two"}}
